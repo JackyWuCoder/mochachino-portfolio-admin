b4c0d654fb22971b857cca55c58291205f47cc6d
Jacky Wu <JackyWuCoder@gmail.com>
2026-03-25 21:59:31 -0700

    feat: request database for SAS upload URL + public URL

--- script.js
+++ script.js
@@ -16,16 +16,40 @@ featuredImageButtons.forEach((button) => {
 
 featuredImageInput.addEventListener("change", async (event) => {
   const file = event.target.files[0];
-
   if (!file || !activeButton) return;
 
   const slot = Number(activeButton.dataset.slot);
+  if (![1, 2, 3, 4].includes(slot)) return;
+
   console.log("Slot: ", slot);
   console.log("Selected file:", file);
 
+  // temporary Local preview
   const previewUrl = URL.createObjectURL(file);
   activeButton.querySelector("img").src = previewUrl;
 
   // TODO: upload + db update here
+  try {
+    // 1) ask backend for SAS upload URL + public URL
+    const fileExt = file.name.split(".").pop()?.toLowerCase() || "jpg";
+    const sasRes = await fetch(
+      "http://localhost:8000//api/uploads/featured-image-sas",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`, // adjust if needed
+        },
+        body: JSON.stringify({ slot, fileExt }),
+      },
+    );
+
+    if (!sasRes.ok) {
+      throw new Error("Failed to get upload URL");
+    }
+
+    const { uploadUrl, publicUrl } = await sasRes.json();
+  } catch (error) {}
+
   featuredImageInput.value = "";
 });
